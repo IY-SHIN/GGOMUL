44cb96e0202b80eedc0755508ca3b79a72b0c910
SHIN IN YOUNG <inyoung135@hanmail.net>
2022-06-06 22:56:09 +0900

    - 헤더 수정

--- ggomul/src/components/Header.js
+++ ggomul/src/components/Header.js
@@ -1,20 +1,26 @@
-import React from "react";
-import {GoThreeBars} from 'react-icons/go';
-import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import {useNavigate } from "react-router-dom";
 
 const Header = () => {
     const navigate = useNavigate();
+    const [click, setClick] = useState(false);
+    const handleClick = () =>setClick(!click);
 
     return (
     <div className="header">
+
+        <i className="fas fa-code"></i>
         
         <div className="logo">GGOMUL</div>
     
         <div className="header_option">
-        <span className="login" onClick={() => navigate("/login")}>로그인</span>
-        <span className="join" onClick={() => navigate("/")}>회원가입</span>
-        <span className="icon"><GoThreeBars/></span>
+        <span  className="header-links" onClick={() => navigate("/login")}>Login</span>
+        <span  className="header-links" onClick={() => navigate("/register")}>Join</span>
         </div>
+
+        <span className="nav-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times":"fas fa-bars"}></i>
+        </span>
 
     </div>
         
